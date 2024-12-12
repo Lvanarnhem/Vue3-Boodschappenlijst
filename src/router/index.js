f0365687cbb35a04bb760fcab9987015cb518c56
Lvanarnhem <larissa.va@live.nl>
2024-12-12 12:31:40 +0100

    create router toegevoegd

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,15 @@
 import  "./domains/groceries/pages/Overview.vue"
 import "domains/pages/Create.vue"
 import  "domains/pages/Edit.vue"
+import { createMemoryHistory, createRouter } from 'vue-router'
 
 const routes = [
     { path: '/overview', component: Overview },
     { path: '/create', component: Create },
     { path: '/edit', component: Edit },
   ]
+
+  const router = createRouter({
+    history: createMemoryHistory(),
+    routes,
+  })
